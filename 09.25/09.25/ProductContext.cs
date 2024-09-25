c7930eedff34c82dfea5e31661c2bb796b911aee
baratadam <baratadam06@gmail.com>
2024-09-25 08:17:56 +0200

    .0

---
+++ 09.25/09.25/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._25
+{
+    public class ProductContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+
+
+
---
+++ 09.25/09.25/Sale.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._25
+{
+    public class Sale
+    {
+        public int SalesDate { get; set; }
+        public List<Product> Products { get; set; } = [];
+    }
+}
